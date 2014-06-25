a8136ff66b2925cebc06dd292ce8bf7ef8e990fe
guerot_a <guerot_a@epitech.eu>
2014-06-25 15:04:33 +0200

    Ajout du dossier server (avec sockstream)

---
+++ server/include/socketstream.h
@@ -0,0 +1,33 @@
+#ifndef SOCKETSTREAM_H
+# define SOCKETSTREAM_H
+
+# include <stdbool.h>
+
+# define SOCKETSTREAM_BUFFER_SIZE	4096
+
+typedef struct	s_socketstream
+{
+  int		socket;
+  int		begin_input;
+  int		size_input;
+  char		buffer_input[SOCKETSTREAM_BUFFER_SIZE];
+  int		begin_output;
+  int		size_output;
+  char		buffer_output[SOCKETSTREAM_BUFFER_SIZE];
+}		t_socketstream;
+
+/* METHODS */
+
+void		socketstream_initialize(t_socketstream* this, int socket);
+void		socketstream_release(t_socketstream* this);
+int		socketstream_read(t_socketstream* this, char* buffer, int size);
+int		socketstream_write(t_socketstream* this, char* buffer, int size);
+bool		socketstream_flush_output(t_socketstream* this);
+bool		socketstream_flush_input(t_socketstream* this);
+
+/* OPERATORS */
+
+t_socketstream*		socketstream_new(int socket);
+void			socketstream_delete(t_socketstream* sockstream);
+
+#endif /* SOCKETSTREAM_H */
