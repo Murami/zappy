62b53ddbce70c1b8feeb0f1890c0d83c9775b4eb
pinon_a <pinon_a@epitech.eu>
2014-07-07 15:49:37 +0200

    .

--- ia/zappyNetwork.py
+++ ia/zappyNetwork.py
@@ -1,7 +1,4 @@
 import socket
-import sys
-import encodings.idna
-import unicodedata
 
 class   ZappyNetwork:
 
@@ -9,8 +6,8 @@ class   ZappyNetwork:
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((ip, port))
-        except socket.error:
-            print("Network Error: connecton failure\n");
+        except socket.error as serr:
+            print(serr);
 
     def __del__ (self):
         self.sock.close()
