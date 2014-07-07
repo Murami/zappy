eb90773865a9f97967f618f4ee158f8e14576cb1
pinon_a <pinon_a@epitech.eu>
2014-07-07 15:38:48 +0200

    merge

--- ia/ia.py
+++ ia/ia.py
@@ -3,18 +3,14 @@ import inventory
 import sys
 
 class   Player:
-    """Ceci est la classe player, qui gère un joueur sur la map"""
 
     def __init__ (self, teamName):
         self.teamName = teamName
-        self.net = zappyNetwork.ZappyNetwork(sys.argv[1], sys.argv[2])
+        self.net = zappyNetwork.ZappyNetwork(sys.argv[1], int(sys.argv[2]))
         self.net.send(teamName)
 
     def sendMessageToServer (self, msg):
         self.net.send(msg)
-
-################################################################## fin de la classe
-
 
 def main():
     try:
@@ -22,9 +18,6 @@ def main():
     except IndexError:
         print("\033[31mError in connect : "
               + "\033[33mmissing argument(s)\033[0m")
-
-
-    # demander au serveur le contenu de l'inventaire coute du temps, mais une fois que c'est fait on replit notre classe inventaire côté client et on peut y accèder plein de fois
 
 
 
--- ia/zappyNetwork.py
+++ ia/zappyNetwork.py
@@ -3,27 +3,20 @@ import sys
 import encodings.idna
 import unicodedata
 
-### ceci est la classe que doit me fournir pinon, en mieux bien sûr ;)
-
 class   ZappyNetwork:
-    """Classe qui sera appelé par les joueurs lors de l'envoie de données"""
 
     def __init__ (self, ip, port):
-        self.ip = ip
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((ip, port))
+        except socket.error:
+            print("Network Error: connecton failure\n");
 
     def __del__ (self):
         self.sock.close()
 
-    def connect(self, teamName):
-        self.sock.connect((self.ip, int(self.port)))
-        self.sock.send(bytes(teamName + "\n", "utf-8"))
-        receive = str(self.sock.recv(1024), "utf-8")
-        print("HERE : ", receive)
-        if receive  == "ko\n":
-            print("Error")
+    def send (self, msg):
+        self.sock.send(msg + "\n")
 
-    def communicate (self, msg):
-        self.sock.send(bytes(msg + "\n", "utf-8"))
-        return str(self.sock.recv(1024), "utf-8")
+    def recv (self):
+        return self.sock.recv(42420)
