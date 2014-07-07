da7954d16d56a9d3367f3f22ca2c52a28368b6c1
bichon_b <bichon_b@epitech.eu>
2014-07-07 15:55:57 +0200

    .

--- ia/ia.py
+++ ia/ia.py
@@ -3,10 +3,13 @@ import inventory
 import sys
 
 class   Player:
-
     def __init__ (self, teamName):
         self.teamName = teamName
-        self.net = zappyNetwork.ZappyNetwork(sys.argv[1], int(sys.argv[2]))
+        try:
+            self.net = zappyNetwork.ZappyNetwork(sys.argv[1], sys.argv[2])
+        except IndexError:
+            print("\033[31mError in connect : "
+                  + "\033[33mmissing argument(s)\033[0m")
         self.net.send(teamName)
 
     def sendMessageToServer (self, msg):
@@ -20,5 +23,7 @@ def main():
               + "\033[33mmissing argument(s)\033[0m")
 
 
-
-main()
+try:
+    main()
+except:
+    print("\033[32mSo I quit...\033[0m")
