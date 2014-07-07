5fb51d72a16a3cc4bed7d86be6c353821cdc0adf
bichon_b <bichon_b@epitech.eu>
2014-07-07 15:37:45 +0200

    .

--- ia/ia.py
+++ ia/ia.py
@@ -7,60 +7,25 @@ class   Player:
 
     def __init__ (self, teamName):
         self.teamName = teamName
-
-    # listes des commandes
-    def connect (self):
         self.net = zappyNetwork.ZappyNetwork(sys.argv[1], sys.argv[2])
-        self.net.connect(self.teamName)
+        self.net.send(teamName)
 
-    def forward (self):
-        self.net.communicate("avance")
-
-    def turnRight (self):
-        self.net.communicate("droite")
-
-    def turnLeft (self):
-        self.net.communicate("gauche")
-
-    def see (self):
-        self.net.communicate("voir")
-
-    def inventory (self):
-        self.net.communicate("inventaire")
-
-    def takeObject (self, objectName):
-        self.net.communicate("prend {0}".format(objectName))
-
-    def placeObject (self, objectName):
-        res = self.net.communicate("pose {0}".format(objectName))
-        print(res)
-
-    def eject (self):
-        self.net.communicate("expulse")
-
-    def broadcast (self, msg):
-        self.net.communicate("broadcast " + msg)
-
-    def incantation (self):
-        self.net.communicate("incantation")
-
-    def fork(self):
-        self.net.communicate("fork")
-
-    def freeSlotNbr (self):
-        self.net.communicate("connect_nbr")
+    def sendMessageToServer (self, msg):
+        self.net.send(msg)
 
 ################################################################## fin de la classe
 
 
 def main():
-    one = Player("Bichon")
-    two = Player("Regnier")
-    one.connect()
+    try:
+        player = Player(sys.argv[3])
+    except IndexError:
+        print("\033[31mError in connect : "
+              + "\033[33mmissing argument(s)\033[0m")
+
 
     # demander au serveur le contenu de l'inventaire coute du temps, mais une fois que c'est fait on replit notre classe inventaire côté client et on peut y accèder plein de fois
 
-    one.inventory()
-    one.placeObject("linemate")
+
 
 main()
