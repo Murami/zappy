971bf372698fa2fb4a6525c64d98c01b0426fcb4
desabr_q <desabr_q@epitech.eu>
2014-07-12 07:17:47 +0200

    haha ça compile

--- server/src/monitor/monitors.c
+++ server/src/monitor/monitors.c
@@ -115,7 +115,7 @@ void		monitor_send_eggs(t_gameplay *this, t_client *client)
   while (it != list_end(this->players))
     {
       player = it->data;
-      monitor_send_player(this, player, client);
+      monitor_send_player(player, client);
       it = list_iterator_next(it);
     }
 }
