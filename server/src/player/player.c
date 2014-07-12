928fcf3b97a57eb3340f906096e737fa440f2c59
guerot_a <guerot_a@epitech.eu>
2014-07-12 23:52:25 +0200

    Connect nbr + connexion modifée pour gerer les slot corectement

--- server/src/player/player.c
+++ server/src/player/player.c
@@ -28,7 +28,7 @@ void		player_initialize(t_player *this, t_gameplay *gameplay,
   this->team = team;
   this->is_egg = false;
   this->command_queue = list_new();
-  team->nb_slots--;
+  /* team->nb_slots--; */
   case_initialize(&this->inventory, this->x, this->y);
   this->inventory.food = 10;
   this->client = client;
