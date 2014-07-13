013a498ecf0f2f4959bbce496b7949707d806a36
guerot_a <guerot_a@epitech.eu>
2014-07-13 11:50:00 +0200

    ?

---
+++ server/include/gameplay_misc.h
@@ -0,0 +1,12 @@
+#ifndef GAMEPLAY_MISC_H
+# define GAMEPLAY_MISC_H
+
+bool	check_stone1(t_gameplay* this, int x, int y);
+bool	check_stone2(t_gameplay* this, int x, int y);
+bool	check_stone3(t_gameplay* this, int x, int y);
+bool	check_stone4(t_gameplay* this, int x, int y);
+bool	check_stone5(t_gameplay* this, int x, int y);
+bool	check_stone6(t_gameplay* this, int x, int y);
+bool	check_stone7(t_gameplay* this, int x, int y);
+
+#endif /* GAMEPLAY_MISC_H */
